2fcecd53fb233c3e87e6b01c7ae33ad484b14c5a
Godbobo <980742324@qq.com>
2019-10-25 13:54:16 +0800

    上传时忽略unpackage目录

--- unpackage/dist/dev/app-plus/pages/cpt/camera.js
+++
@@ -1,2 +0,0 @@
-setCssToHead(["@charset \x22UTF-8\x22;\n.",[1],"btn-wrap.",[1],"data-v-3cf3217e { padding: 20px ",[0,20],"; }\n",],undefined,{path:"./pages/cpt/camera.wxss"})();
-document.dispatchEvent(new CustomEvent("generateFuncReady", { detail: { generateFunc: $gwx('./pages/cpt/camera.wxml') } }));
